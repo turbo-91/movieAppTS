0bbc89d609e5f3f09a2ef03088579ec4aba30ef4
Torben Jost <torbenjost@proton.me>
2025-02-05 14:05:57 +0100

    get movies from APIs

---
+++ src/lib/idToImg.ts
@@ -0,0 +1,7 @@
+export function idToImg(imdbLink: string) {
+  const imdbKey = process.env.NEXT_PUBLIC_TMSB_KEY;
+  const parts = imdbLink.split("/");
+  const imdbId = parts.find((part) => part.startsWith("tt"));
+  const imdbImgLink = `https://api.themoviedb.org/3/find/${imdbId}?api_key=${imdbKey}&language=de&external_source=imdb_id`;
+  return imdbImgLink;
+}
