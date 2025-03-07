eab08d36dc34b12956e75030b3022c8e55bffcb2
Torben Jost <torbenjost@proton.me>
2025-03-06 21:13:22 -0300

    fix movies of the day functionality

--- src/db/models/Movie.ts
+++ src/db/models/Movie.ts
@@ -4,10 +4,10 @@ export interface Movie extends Document {
   netzkinoId: number;
   slug: string;
   title: string;
-  year: string;
+  year: [string];
   overview: string;
-  regisseur: string;
-  stars: string;
+  regisseur: [string];
+  stars: [string];
   imgNetzkino: string;
   imgNetzkinoSmall: string;
   imgImdb: string;
@@ -15,14 +15,13 @@ export interface Movie extends Document {
   dateFetched?: string[];
 }
 
-// Define Schema with TypeScript
 const movieSchema = new Schema<Movie>({
   netzkinoId: { type: Number, required: true },
   slug: { type: String, required: true },
   title: { type: String, required: true },
-  year: { type: String, required: true },
-  regisseur: { type: String, required: true },
-  stars: { type: String, required: true },
+  year: { type: [String], required: true },
+  regisseur: { type: [String], required: true },
+  stars: { type: [String], required: true },
   overview: { type: String, required: true },
   imgNetzkino: { type: String, required: true },
   imgNetzkinoSmall: { type: String, required: true },
