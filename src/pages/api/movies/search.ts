17908453546cc65a822676152c52e160210181c7
Torben Jost <torbenjost@proton.me>
2025-03-18 10:49:16 +0100

    build search api route

---
+++ src/pages/api/movies/search.ts
@@ -0,0 +1,28 @@
+import dbConnect from "@/db/mongodb";
+import handleApiError from "@/lib/handleApiError";
+import { getSearchMovies } from "@/services/movieService";
+import type { NextApiRequest, NextApiResponse } from "next";
+
+export default async function getSearchResults(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  await dbConnect();
+
+  try {
+    const { query } = req.query;
+    if (!query || typeof query !== "string") {
+      return res.status(400).json({ error: "Query parameter is required" });
+    }
+
+    const movies = await getSearchMovies(query);
+
+    if (!movies || movies.length === 0) {
+      return res.status(404).json({ status: "Not Found" });
+    }
+
+    return res.status(200).json(movies);
+  } catch (error) {
+    return handleApiError(res, "Error fetching movies", error);
+  }
+}
