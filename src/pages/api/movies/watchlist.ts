fd3d76f94eb49d709fc422d17487c2d169467d83
Torben Jost <torbenjost@proton.me>
2025-03-27 09:09:59 +0100

    build watchlist GET route

--- src/pages/api/movies/watchlist.ts
+++ src/pages/api/movies/watchlist.ts
@@ -1,6 +1,34 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import dbConnect from "@/db/mongodb";
+import { getMoviesByUser } from "@/services/watchlistService";
+import handleApiError from "@/lib/handleApiError";
+
 export default async function watchlistHandler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  console.log("ur mom");
+  await dbConnect();
+  switch (req.method) {
+    case "GET":
+      try {
+        const { userid } = req.query;
+        if (!userid || typeof userid !== "string") {
+          return res
+            .status(400)
+            .json({ error: "UserId parameter is required" });
+        }
+
+        const movies = await getMoviesByUser(userid);
+
+        if (!movies || movies.length === 0) {
+          return res.status(404).json({ status: "Not Found" });
+        }
+
+        return res.status(200).json(movies);
+      } catch (error) {
+        return handleApiError(res, "Error fetching movies", error);
+      }
+    default:
+      return res.status(405).json({ status: "Method Not Allowed" });
+  }
 }
