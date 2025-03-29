1e107ef5599810a111c176b6bbe97162add26f19
Torben Jost <torbenjost@proton.me>
2025-03-29 15:11:02 +0100

    style header

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,22 +1,16 @@
 import React from "react";
 import Head from "next/head";
 import Header from "./Header";
-import { Montserrat } from "next/font/google";
-
-const montserrat = Montserrat({
-  subsets: ["latin"],
-  variable: "--font-montserrat",
-});
 
 const Layout = ({ children }) => {
   return (
-    <div className={montserrat.variable}>
+    <>
       <Head>
         <title>MovieApp</title>
       </Head>
       <Header />
       <main>{children}</main>
-    </div>
+    </>
   );
 };
 
