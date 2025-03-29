b9da59950cc0d209269b31447aeb3ef1390f638f
Torben Jost <torbenjost@proton.me>
2025-03-29 15:11:02 +0100

    style header

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,15 +1,42 @@
 import React from "react";
 import { useState } from "react";
 import NavBar from "./NavBar";
+import styled from "styled-components";
+
+const HeaderContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: flex-end;
+  border-bottom: 0.2rem solid white;
+  color: white;
+  font-weight: 100;
+  width: 100%;
+  margin: 0;
+  padding: 0;
+`;
+
+const AppTitle = styled.h1`
+  font-size: 3.5rem;
+  font-weight: var(--font-weight-semibold);
+  margin: 0;
+  padding: 0.7rem;
+`;
+
+const Menu = styled.h3`
+  font-size: 1rem;
+  font-weight: var(--font-weight-light);
+  margin-bottom: 0;
+  padding: 0.7rem;
+`;
 
 export default function Header() {
   const [menu, setMenu] = useState<boolean>(false);
 
   return (
-    <div>
-      <h1>MovieApp</h1>
-      <h1 onClick={() => setMenu((prevMenu) => !prevMenu)}>Menu</h1>
+    <HeaderContainer>
+      <AppTitle>Movie App</AppTitle>
+      <Menu onClick={() => setMenu((prevMenu) => !prevMenu)}>Menü</Menu>
       {menu && <NavBar />}
-    </div>
+    </HeaderContainer>
   );
 }
--- src/styles/GlobalStyles.ts
+++ src/styles/GlobalStyles.ts
@@ -1,15 +1,33 @@
 import { createGlobalStyle } from "styled-components";
 import { Noto_Sans } from "next/font/google";
 
-const montserrat = Noto_Sans({ subsets: ["latin"] });
+const notoSans = Noto_Sans({
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700"],
+});
 
 const GlobalStyle = createGlobalStyle`
- body {
- background-color: black;   
- margin: 0;
-    font-family: ${montserrat.style.fontFamily}; 
+  :root {
+  font-size: 16px;
+    --font-family: ${notoSans.style.fontFamily};
+    --font-weight-light: 300;
+    --font-weight-regular: 400;
+    --font-weight-medium: 500;
+    --font-weight-semibold: 600;
+    --font-weight-bold: 700;
+  }
+
+  *, *::before, *::after {
+    box-sizing: border-box;
+  }
+
+  body {
+    background-color: black;
+    margin: 0;
+    padding: 0;
+    font-family: var(--font-family);
     color: white;
-    padding: 2rem;
+    font-weight: var(--font-weight-regular); /* default */
   }
 `;
 
