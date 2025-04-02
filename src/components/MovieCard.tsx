7c7c56f3763a9a11b9893a94788ce71f1d4257db
Torben Jost <torbenjost@proton.me>
2025-04-02 10:23:42 +0200

    style MovieCard

--- src/components/MovieCard.tsx
+++ src/components/MovieCard.tsx
@@ -5,6 +5,65 @@ import Image from "next/image";
 import { useWatchlist } from "@/lib/hooks/useWatchlist";
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/router";
+import { Icon, Star } from "lucide-react";
+import styled from "styled-components";
+
+const CardWrapper = styled.div`
+  position: relative;
+
+  &:hover .hover-info {
+    opacity: 1;
+  }
+`;
+
+const HoverInfo = styled.div`
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  width: 100%;
+  padding: 0.5rem;
+  background: rgba(0, 0, 0, 0.6);
+  color: white;
+  opacity: 0;
+  transition: opacity 0.3s ease;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const TitleRow = styled.div`
+  display: flex;
+  justify-content: start;
+  align-items: center;
+  width: 100%;
+`;
+
+const Title = styled.h1`
+  font-size: 1rem;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+  margin: 0;
+  padding: 0.3rem;
+`;
+
+const Year = styled.p`
+  margin-left: auto;
+  padding: 0.3rem;
+  white-space: nowrap;
+`;
+
+const IconWrapper = styled.div`
+  position: absolute;
+  top: 20px;
+  right: 20px;
+`;
+
+const WatchlistButton = styled.button`
+  all: unset;
+  cursor: pointer;
+  font-size: 1.2rem;
+`;
 
 export interface MovieCardProps {
   key: number;
@@ -33,32 +92,40 @@ export default function MovieCard(props: Readonly<MovieCardProps>) {
   }
 
   return (
-    <div>
+    <CardWrapper>
+      <IconWrapper>
+        {isInWatchlist ? (
+          <WatchlistButton
+            onClick={() =>
+              removeFromWatchlist(() => {
+                router.replace(router.asPath);
+              })
+            }
+          >
+            <Star fill="white" size={35} strokeWidth={1} />
+          </WatchlistButton>
+        ) : (
+          <WatchlistButton onClick={addToWatchlist}>
+            <Star size={35} strokeWidth={1} />
+          </WatchlistButton>
+        )}
+      </IconWrapper>
+      <HoverInfo className="hover-info">
+        <TitleRow>
+          <Title>{movie.title}</Title>
+          <Year>{movie.year}</Year>
+        </TitleRow>
+      </HoverInfo>
       <div onClick={() => onClick(movie)}>
-        <h2>{movie.title}</h2>
-        <p>{movie.year}</p>
         <Image
           loader={customLoader}
           src={imageSrc}
           alt={movie.title}
-          width={600}
-          height={200}
+          width={450}
+          height={225}
           onError={() => setHasError(true)}
         />
       </div>
-      {isInWatchlist ? (
-        <button
-          onClick={() =>
-            removeFromWatchlist(() => {
-              router.replace(router.asPath);
-            })
-          }
-        >
-          Remove from Watchlist
-        </button>
-      ) : (
-        <button onClick={addToWatchlist}>Add to Watchlist</button>
-      )}
-    </div>
+    </CardWrapper>
   );
 }
